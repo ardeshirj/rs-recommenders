c161cf09f674738cd8af6a695806e1979e72c188
ardeshirj <ardeshir.javaherchi@gmail.com>
2016-01-24 12:14:26 -0800

    Add find item spec

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,16 +1,21 @@
 require 'spec_helper'
 
 describe Item do
-  # item = Item.new(1157, '26 Greenline Womens BC-109')
+  describe '.find_item' do
+    it 'should the item matching the item id' do
+      expected = Item.new(1_298, 'Throttle Cable')
+      expect(Item.find_item(1_298)).to eq(expected)
+    end
+  end
 
   describe '.find_all_items' do
-    it 'return all items from database' do
+    it 'should return all items from database' do
       expect(Item.find_all_items.count).to eq(474)
     end
   end
 
   describe '.find_purchased_items' do
-    it 'return all items purchased by the user from database' do
+    it 'should return all items purchased by the user from database' do
       expect(Item.find_purchased_items(35_914).count).to eq(6)
     end
   end
