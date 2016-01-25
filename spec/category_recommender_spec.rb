7bff876d02eb6be1f2a2a39ece71310a8f8e815c
ardeshirj <ardeshir.javaherchi@gmail.com>
2016-01-25 13:23:42 -0800

    Add comment

--- spec/category_recommender_spec.rb
+++ spec/category_recommender_spec.rb
@@ -3,11 +3,14 @@ require 'spec_helper'
 describe CategoryRecommender do
   before :all do
     user = User.find_user(35_914)
+    # user category_ids = [134, 152, 133, 129, 164, 168, 154]
+    # user item_ids     = [1253, 1532, 1532, 1298, 1314, 1366]
 
     inventory = [
-      Item.find_item(1_253),
-      Item.find_item(1_286),
-      Item.find_item(1_423)
+      Item.find_item(1_423),  # category_ids = [154, 164, 168]
+      Item.find_item(1_286),  # category_ids = [168]
+      Item.find_item(1_253),  # category_ids = [134, 152]
+      Item.find_item(1_170)   # category_ids = [172]
     ]
 
     @recommender = CategoryRecommender.new(user, inventory)
