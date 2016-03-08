c4d94a371f9766057acb327fe7ca16eda705eba5
ardeshirj <ardeshir.javaherchi@gmail.com>
2016-03-07 16:52:32 -0800

    Add rakefile

---
+++ Rakefile.rb
@@ -0,0 +1,12 @@
+require 'rspec/core/rake_task'
+
+task default: %w(create_db spec)
+
+task :create_db do
+  sh 'rm rs-db'
+  sh 'sqlite3 rs-db < create-db.sql'
+end
+
+RSpec::Core::RakeTask.new(:spec) do |t|
+  t.pattern = 'spec'
+end
