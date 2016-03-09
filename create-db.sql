b4a2661b4999918bf7983ef4f2491b3082642715
ardeshirj <ardeshir.javaherchi@gmail.com>
2016-03-09 15:08:50 -0800

    Remove sh command & add create table commands

--- create-db.sql
+++ create-db.sql
@@ -1,5 +1,11 @@
 .separator "\t"
 
+create table item (item_id INTEGER, name TEXT);
+create table user (user_id INTEGER, name TEXT);
+create table item_user (user_id INTEGER, item_id INTEGER);
+create table category (category_id INTEGER, name TEXT);
+create table item_category (item_id INTEGER, category_id INTEGER);
+
 .import raw_data/mini_proj-items.csv item
 .import raw_data/mini_proj-users.csv user
 .import raw_data/mini_proj-items_users.csv item_user
